9e714df740e516b2eed3e401b48fa379bbe40cbc
NickOvt <nikolai@zone.ee>
2025-07-21 20:40:15 +0300

    add ChunkedPassthrough to collect 64kb chunks before splitting data

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const MessageJoiner = require('./lib/message-joiner');
 const NodeRewriter = require('./lib/node-rewriter');
 const NodeStreamer = require('./lib/node-streamer');
 const Headers = require('./lib/headers');
+const ChunkedPassthrough = require('./lib/chunked-passthrough');
 
 module.exports = {
     Splitter: MessageSplitter,
     Joiner: MessageJoiner,
     Rewriter: NodeRewriter,
     Streamer: NodeStreamer,
+    ChunkedPassthrough,
     Headers
 };
